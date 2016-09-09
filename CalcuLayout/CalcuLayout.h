978c766eb9e4a6cbe1ea1427d7536a45fad1ee1a
magfurulabeer <magfurulabeer@gmail.com>
2016-09-09 03:47:22 -0400

    Initial Commit

---
+++ CalcuLayout/CalcuLayout.h
@@ -0,0 +1,19 @@
+//
+//  CalcuLayout.h
+//  CalcuLayout
+//
+//  Created by Magfurul Abeer on 9/9/16.
+//  Copyright © 2016 Magfurul Abeer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CalcuLayout.
+FOUNDATION_EXPORT double CalcuLayoutVersionNumber;
+
+//! Project version string for CalcuLayout.
+FOUNDATION_EXPORT const unsigned char CalcuLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalcuLayout/PublicHeader.h>
+
+
